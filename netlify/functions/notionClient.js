6b68aaf76c920164282ffdb293f0ef7a9c60f2e0
Constant V <constantvallee@gmail.com>
2022-07-06 23:44:17 -0400

    add try catch

--- netlify/functions/notionClient.js
+++ netlify/functions/notionClient.js
@@ -3,20 +3,37 @@ const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: 'secret_Iv5KchMYDUaqiOV4RB1oa0kNxYHiKfwIRxnMb1lHAht' });
 
 exports.handler = async (event, context) => {
-    const response = await notion.pages.create({
-        parent:{ 
-            database_id: "797daf87bfd5422f9abae33dc1084bab"
-          },
-          propertiers: {
-            Name: {
-              title: [
-                {
-                  text: {
-                    content: "It work",
-                  }
-                }
-              ]
-            },
+    let response
+    try {
+        response = await notion.pages.create({
+            parent:{ 
+                database_id: "797daf87bfd5422f9abae33dc1084bab"
+              },
+              propertiers: {
+                Name: {
+                  title: [
+                    {
+                      text: {
+                        content: "It work",
+                      }
+                    }
+                  ]
+                },
+              }
+        })
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+              error: err.message
+            })
           }
-    })
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: response
+        })
+    }
   }
